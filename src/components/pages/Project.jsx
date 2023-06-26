1649ff6a36432dc370637e537a0006ba4cc6e5d8
AltafFattah <altaf.fattah05@gmail.com>
2023-06-26 12:17:42 +0700

    Rapihin Code/

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -13,7 +13,10 @@ const Project = ({ id }) => {
 
   return (
     <div>
-      <section id={id} className="container w-full h-auto p-4 mx-auto mt-20 md:p-0 lg:p-4">
+      <section
+        id={id}
+        className="container w-full h-auto p-4 mx-auto mt-20 md:p-0 lg:p-4"
+      >
         <div className="w-[90%] mx-auto text-center ">
           <AosCom dataAos="fade" dataAosOnce="true">
             <h1 className="text-2xl font-bold text-slate-900">Project</h1>
@@ -22,10 +25,13 @@ const Project = ({ id }) => {
             </p>
           </AosCom>
         </div>
-        
+
         <div className="flex flex-col flex-wrap md:justify-start lg:justify-center  items-start w-full py-4 mx-auto mt-9 bg-red-500 gap-x-3  gap-7 md:flex-row md:w-[100%] xl:w-[80%]  xl:gap-14">
-          {projects.map((project,i) => (
-            <div key={i} className="w-full  h-full rounded-2xl md:w-[20rem] lg:w-[29rem] mx-auto md:mx-5 lg:mx-0 ">
+          {projects.map((project, i) => (
+            <div
+              key={i}
+              className="w-full  h-full rounded-2xl md:w-[20rem] lg:w-[29rem] mx-auto md:mx-5 lg:mx-0 "
+            >
               <AosCom
                 dataAos={project.dataAos}
                 dataAosDuration={project.dataAosDuration}
@@ -38,9 +44,13 @@ const Project = ({ id }) => {
                       <img src="" alt="" />
                     </div>
                     <div className=" w-[90%] mx-auto my-1">
-                      <h1 className="text-xl font-semibold text-slate-800">{project.name}</h1>
+                      <h1 className="text-xl font-semibold text-slate-800">
+                        {project.name}
+                      </h1>
                       <p className="text-slate-500 group w-[30%]">
-                        <a href="" className="text-base font-medium">{project.demo}</a>
+                        <a href="" className="text-base font-medium">
+                          {project.demo}
+                        </a>
                         <BsArrowRightShort className="inline-block ml-1 text-2xl font-extrabold transition-all ease-linear group-hover:translate-x-1" />
                       </p>
                     </div>
@@ -49,66 +59,7 @@ const Project = ({ id }) => {
               </AosCom>
             </div>
           ))}
-
-          {/* <div className='w-full  h-full rounded-2xl md:w-[26.4rem] mx-auto md:mx-0'>
-                        <AosCom dataAos="fade" dataAosDuration={400} dataAosDelay={50} dataAosOnce="true">
-                        <div className='w-full bg-white h-full rounded-2xl md:w-[26.4rem] mx-auto md:mx-0 py-1 border'>
-                            <div >
-                                <div className='w-[90%] bg-orange-500 h-[13rem] mx-auto mt-5 rounded-2xl md:h-[15rem]'>
-                                    <img src="" alt="" />
-                                </div>
-                                <div className=' w-[90%] mx-auto my-3'>
-                                    <h1 className='text-xl font-semibold text-slate-800'>Name Project</h1>
-                                    <p className='text-slate-500 group w-[30%]'>
-                                        <a href="" className='text-base font-medium'>Demo</a>
-                                        <BsArrowRightShort className='inline-block ml-1 text-2xl font-extrabold transition-all ease-linear group-hover:translate-x-1' />
-                                    </p>
-                                </div>
-                            </div>
-                            </div>
-                        </AosCom>
-                    </div>
-
-                    <div className='w-full h-full rounded-2xl md:w-[26.4rem] mx-auto md:mx-0'>
-                        <AosCom dataAos="fade" dataAosDuration={500} dataAosDelay={70} dataAosOnce="true">
-                    <div className='w-full bg-white h-full rounded-2xl md:w-[26.4rem] mx-auto md:mx-0 py-1 border'>
-                            <div >
-                                <div className='w-[90%] bg-orange-500 h-[13rem] mx-auto mt-5 rounded-2xl md:h-[15rem]'>
-                                    <img src="" alt="" />
-                                </div>
-                                <div className=' w-[90%] mx-auto my-3'>
-                                    <h1 className='text-xl font-semibold text-slate-800'>Name Project</h1>
-                                    <p className='text-slate-500 group w-[30%]'>
-                                        <a href="" className='text-base font-medium'>Demo</a>
-                                        <BsArrowRightShort className='inline-block ml-1 text-2xl font-extrabold transition-all ease-linear group-hover:translate-x-1' />
-                                    </p>
-                                </div>
-                            </div>
-                            </div>
-                        </AosCom>
-                    </div>
-
-
-                    <div className='w-full h-full rounded-2xl md:w-[26.4rem] mx-auto md:mx-0'>
-                        <AosCom dataAos="fade" dataAosDuration={600} dataAosDelay={80} dataAosOnce="true">
-                            <div className=' bg-white h-full rounded-2xl md:w-[26.4rem] mx-auto md:mx-0 py-1 border'>
-                                <div>
-                                    <div className='w-[90%] bg-orange-500 h-[13rem] mx-auto mt-5 rounded-2xl md:h-[15rem]'>
-                                        <img src="" alt="" />
-                                    </div>
-                                    <div className=' w-[90%] mx-auto my-3'>
-                                        <h1 className='text-xl font-semibold text-slate-800'>Name Project</h1>
-                                        <p className='text-slate-500 group w-[30%]'>
-                                            <a href="" className='text-base font-medium'>Demo</a>
-                                            <BsArrowRightShort className='inline-block ml-1 text-2xl font-extrabold transition-all ease-linear group-hover:translate-x-1' />
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </AosCom>
-                    </div> */}
         </div>
-        ;
       </section>
     </div>
   );
