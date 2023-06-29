19fed896e87232935eaf832f8e943b17c3302434
AltafFattah <altaf.fattah05@gmail.com>
2023-06-29 22:24:21 +0700

    Navbar

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -3,7 +3,7 @@ import AosCom from '../../../utils/AosCom'
 
 const Navbar = ({ home, about, skills, project }) => {
     return (
-        <nav className="fixed z-50 w-16 h-auto bottom-10 right-1 md:w-20 md:h-96">
+        <nav className="fixed z-50 w-16 h-auto bottom-10 right-5 md:w-20 md:h-96">
             <AosCom dataAos="fade-left" dataAosOnce="true">
                 <div className="flex flex-col items-center justify-center h-full text-center bg-transparent border rounded-full gap-y-3 backdrop-blur-xl border-slate-300">
                     <div className="bg-slate-500 w-[0.12rem] h-10 rounded-e-full"></div>
