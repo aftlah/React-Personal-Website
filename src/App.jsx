f56a163599118fd8ddf070a7b26067ab7aca58c5
AltafFattah <altaf.fattah05@gmail.com>
2023-06-29 22:15:30 +0700

    Fixed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React from "react"
-import Navbar from "./components/pages/Navbar"
-import Home from "./components/pages/Home"
-import About from "./components/pages/About"
-import Skills from "./components/pages/Skills"
-import Project from "./components/pages/Project"
-
+import React from "react";
+import Navbar from "./components/pages/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Skills from "./components/pages/Skills";
+import Project from "./components/pages/Project";
 
 function App() {
-
   return (
-    <div className="bg-slate-50">
-    <Navbar home={"#home"} about={"#about"} skills={"#skills"} project={"#project"}/>
-    <Home id={"home"}/>
-    <About id={"about"}/>
-    <Skills  id={"skills"}/>
-    <Project id={"project"}/>
+    <div className="relative bg-slate-50">
+      <Navbar
+        home={"#home"}
+        about={"#about"}
+        skills={"#skills"}
+        project={"#project"}
+      />
+      <Home id={"home"} />
+      <About id={"about"} />
+      <Skills id={"skills"} />
+      <Project id={"project"} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
